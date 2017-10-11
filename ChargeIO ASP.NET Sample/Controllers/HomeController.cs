2648250f3d89a3cf1463a0e14c95f958b75b0e79
Victor Ude <vude@affinipay.com>
2017-10-11 13:47:19 -0500

    adding a sample app for ASP.NET projects

---
+++ ChargeIO ASP.NET Sample/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web.Mvc;
+using ChargeIO.Services.Merchant;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace ChargeIOASP.NETSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Merchant()
+        {
+            var service = new MerchantService();
+            try
+            {
+                //TODO: try out API calls here
+                var merchant = service.GetMerchant();
+                ViewBag.Message = JsonConvert.SerializeObject(merchant,
+                    Formatting.Indented, new StringEnumConverter());
+//                ViewBag.Message = JObject.FromObject(merchant).ToString();
+            }
+            catch (Exception exception)
+            {
+                ViewBag.Message = exception.Message;
+            }
+
+            return View();
+        }
+    }
+}
