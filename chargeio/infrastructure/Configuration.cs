7a5cba30b586069dad37f7697100cb8f469c34aa
James Sparrow <james@chargeio.com>
2017-10-19 08:38:48 -0500

    Make load of configuration file appsettings.json optional, provide sensible defaults for API URL and HTTP timeout when configuration is not present.

--- chargeio/infrastructure/Configuration.cs
+++ chargeio/infrastructure/Configuration.cs
@@ -6,7 +6,7 @@ namespace ChargeIO.Infrastructure
 {
 	public static class Configuration
 	{
-        public static IConfigurationRoot Get { get; }
+        public static IConfigurationRoot AppCfg { get; }
 		public static string SecretKey { get; }
 		public static string ApiUrl { get; }
 		public static long HttpTimeout { get; }
@@ -14,14 +14,19 @@ namespace ChargeIO.Infrastructure
 
         static Configuration()
         {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", false, true)
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
                 .AddEnvironmentVariables();
 
-            Get = configuration.Build();
-            SecretKey = Get["ChargeIOSecretKey"];
-            ApiUrl = Get["ChargeIOApiUrl"];
-            HttpTimeout = long.Parse(Get["ChargeIOHTTPTimeout"]);
+            AppCfg = builder.Build();
+            SecretKey = AppCfg["ChargeIOSecretKey"];
+
+            string apiUrl = AppCfg["ChargeIOApiUrl"];
+            ApiUrl = apiUrl != null ? apiUrl : "https://api.chargeio.com/v1";
+
+            long timeout = long.TryParse(AppCfg["ChargeIOHTTPTimeout"], out timeout) ? timeout : 30000;
+            HttpTimeout = timeout;
+
 	        AssemblyVersion = typeof(Configuration).GetTypeInfo().Assembly.GetName().Version;
         }
 	}
